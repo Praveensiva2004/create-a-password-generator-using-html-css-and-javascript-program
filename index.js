4a2a8516eab90cc8adb6e748843e79c51530af3b
Praveen s <166095477+Praveensiva2004@users.noreply.github.com>
2024-08-16 10:59:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById('generate-btn').addEventListener('click', function() {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
+    const passwordLength = 12; // You can change the length here
+    let password = '';
+    
+    for (let i = 0; i < passwordLength; i++) {
+        const randomIndex = Math.floor(Math.random() * chars.length);
+        password += chars[randomIndex];
+    }
+    
+    document.getElementById('password').value = password;
+});
